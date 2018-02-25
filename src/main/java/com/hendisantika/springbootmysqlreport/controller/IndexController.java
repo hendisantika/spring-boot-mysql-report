2def42dc130dea71f17128a9674b4d47a9c0e410
Hendi Santika <hendisantika@gmail.com>
2018-02-25 19:12:46 +0700

    #6 - Add Index Controller

---
+++ src/main/java/com/hendisantika/springbootmysqlreport/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springbootmysqlreport.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mysql-report
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/02/18
+ * Time: 19.12
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping("")
+    public String index() {
+        return "Hello World " + new Date();
+    }
+}
