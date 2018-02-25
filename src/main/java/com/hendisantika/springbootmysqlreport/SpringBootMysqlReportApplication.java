2c8be3432b900674b486a2a33f169b42c72cf768
Hendi Santika <hendisantika@gmail.com>
2018-02-25 19:02:00 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmysqlreport/SpringBootMysqlReportApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootmysqlreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlReportApplication.class, args);
+	}
+}
